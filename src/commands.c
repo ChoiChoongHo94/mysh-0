6fa2716e542882b5645855596e8875baf114ae49
ChoiChoongHo94 <cndgh49@ajou.ac.kr>
2017-10-08 02:46:40 +0900

    201320210/ChoiCHoongHo: I changed ~/mysh-0/src/utils.c,commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,13 +1,16 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
+
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
   // TODO: Fill it!
-  return 0;
+  chdir(argv[1]);
+  return 1;
 }
 
 int do_pwd(int argc, char** argv) {
@@ -15,16 +18,21 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-
-  return 0;
+  char* buf;
+  getcwd(buf,255);
+  printf("%s\n",buf);
+   
+  return 1;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"cd")==0 && argc == 2) return 1;
+  else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"pwd")==0 && argc ==1) return 1;
+  else return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,48 @@
 #include "utils.h"
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  int count,temp=0, start = -1;  
+  *argc = 0;
+  while (1){
+ 	 if(command[count]==' '||command[count]=='\t'||command[count]=='\n')
+		count++;
+	 else if(command[count] == '\0'){
+		(*argc)++;
+		*argv = (char**)malloc(sizeof(char*));
+		(*argv)[0] = (char*)malloc(sizeof(char));
+		(*argv)[0][0] = '\0';
+		 break;}
+	 else {start = 1; temp=count; break;}
+  }
+
+  while(start==1){
+	  if(command[count] == ' '|| command[count] == '\0'||
+		command[count]=='\t'||command[count]=='\n'){
+	     count= temp; *argv=(char**)malloc((*argc)*sizeof(char*)); break;}
+
+ 	  else{ 
+		if(command[count+1] == ' ') {(*argc)++; count += 2;}
+		else if(command[count+1] == '\0'){ (*argc)++; count ++;}
+		else if(command[count+1] != ' ') count++;
+	      }
+  }
+ int i =0; 
+
+  while(start==1){
+	if(command[count] == ' ') count++;
+	else if (command[count] == '\0'||
+		command[count]=='\t'||command[count]=='\n') break;
+
+	else{
+		if(command[count+1] == ' ' || command[count+1] == '\0'){
+	  	  (*argv)[i] = (char*)malloc((count-temp+2) * sizeof(char));
+		  for(int j=0; j < count-temp+1; j++) 
+			(*argv)[i][j]=command[temp+j];
+		  (*argv)[i][count-temp+1] = '\0';
+		  count ++; temp = count+1; i++;}
+		else if (command[count+1] != ' ') count++;}
+  }
 }
